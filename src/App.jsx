f942dd0bba184dc1c184fd8625dc4b28ce715115
Thomas Abas <abasienyenethomas2@gmail.com>
2025-05-18 21:55:30 -0800

    r3f fibre

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
- import { Canvas } from '@react-three/fiber'
-import React from 'react'
- import Experience from './components/Experience'
- const App = () => {
-   return (
-    <Canvas>
-      <Experience />
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+function Box() {
+  const meshRef = useRef();
+
+  useFrame(() => {
+    // This one go run every frame (like animation)
+    if (meshRef.current) {
+      meshRef.current.rotation.y += 0.01; // Turn am small-small
+    }
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  );
+}
+
+function App() {
+  return (
+    <Canvas style={{ height: '100vh', background: '#1a1a1a' }}>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[2, 2, 2]} />
+      <OrbitControls />
+      <Box />
     </Canvas>
-  )
- }
- 
- export default App
+  );
+}
+
+export default App;
